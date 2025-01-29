f388f44e29a287456b8054b9f3ab7fed10a1e5ff
lucasmoraist <luksmnt1101@gmail.com>
2025-01-29 11:33:14 -0300

    feat: add validation annotations to ActivityRequest fields

--- src/main/java/com/fiap/hackaton/domain/dto/activity/ActivityRequest.java
+++ src/main/java/com/fiap/hackaton/domain/dto/activity/ActivityRequest.java
@@ -2,14 +2,22 @@ package com.fiap.hackaton.domain.dto.activity;
 
 import com.fiap.hackaton.domain.enums.Execution;
 import com.fiap.hackaton.domain.enums.TypeActivity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ActivityRequest(
+        @NotBlank(message = "Name is required")
         String name,
+        @NotBlank(message = "Description is required")
         String description,
+        @NotNull(message = "Type is required")
         TypeActivity type,
+        @NotNull(message = "Execution is required")
         boolean inGroup,
         Integer studentsPerGroup,
+        @NotNull(message = "Execution is required")
         Execution execution,
+        @NotNull(message = "Prestige value is required")
         Integer prestigeValue
 ) {
 }
