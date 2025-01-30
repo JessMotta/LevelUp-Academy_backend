680e966fd500c26ca3fdbd9d459bde577962896f
lucasmoraist <luksmnt1101@gmail.com>
2025-01-29 22:02:48 -0300

    feat: add validation constraints for prestige value in ActivityRequest

--- src/main/java/com/fiap/hackaton/domain/dto/activity/ActivityRequest.java
+++ src/main/java/com/fiap/hackaton/domain/dto/activity/ActivityRequest.java
@@ -2,6 +2,8 @@ package com.fiap.hackaton.domain.dto.activity;
 
 import com.fiap.hackaton.domain.enums.Execution;
 import com.fiap.hackaton.domain.enums.TypeActivity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -18,6 +20,8 @@ public record ActivityRequest(
         @NotNull(message = "Execution is required")
         Execution execution,
         @NotNull(message = "Prestige value is required")
+        @Min(value = 10, message = "Prestige value must be greater than 10")
+        @Max(value = 90, message = "Prestige value must be less than 90")
         Integer prestigeValue
 ) {
 }
