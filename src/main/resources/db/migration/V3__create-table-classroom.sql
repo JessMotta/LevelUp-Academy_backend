0890a939c95e1c7745060d483356fc1584721160
lucasmoraist <luksmnt1101@gmail.com>
2025-01-31 22:32:20 -0300

    fix: change day_of_week column type from INTEGER to VARCHAR in classroom table

--- src/main/resources/db/migration/V3__create-table-classroom.sql
+++ src/main/resources/db/migration/V3__create-table-classroom.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS t_lvup_classroom (
     id SERIAL PRIMARY KEY,
     subject VARCHAR(255) NOT NULL,
-    day_of_week INTEGER NOT NULL,
+    day_of_week VARCHAR(20) NOT NULL,
     time_spot INTEGER NOT NULL,
     teacher_id BIGINT,
     FOREIGN KEY (teacher_id) REFERENCES t_lvup_teacher(id) ON DELETE SET NULL
