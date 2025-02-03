386083a5509b2172c0daea84c4f36a2773477610
lucasmoraist <luksmnt1101@gmail.com>
2025-02-03 11:27:03 -0300

    feat: add id field to ActivityClassroom record for better activity identification

--- src/main/java/com/fiap/hackaton/domain/dto/activity/ActivityClassroom.java
+++ src/main/java/com/fiap/hackaton/domain/dto/activity/ActivityClassroom.java
@@ -3,12 +3,14 @@ package com.fiap.hackaton.domain.dto.activity;
 import com.fiap.hackaton.domain.entity.Activity;
 
 public record ActivityClassroom(
+        Long id,
         String name,
         String description,
         Integer prestigeValue
 ) {
     public ActivityClassroom(Activity activity) {
         this(
+                activity.getId(),
                 activity.getName(),
                 activity.getDescription(),
                 activity.getPrestigeValue()
