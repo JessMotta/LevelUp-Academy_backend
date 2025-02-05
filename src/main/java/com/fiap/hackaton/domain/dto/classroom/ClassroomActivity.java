7d131696c596eafb896c2f0fb0615b7cafe782a9
lucasmoraist <luksmnt1101@gmail.com>
2025-02-05 15:45:15 -0300

    fix: adding description in ClassroomActivity returning

--- src/main/java/com/fiap/hackaton/domain/dto/classroom/ClassroomActivity.java
+++ src/main/java/com/fiap/hackaton/domain/dto/classroom/ClassroomActivity.java
@@ -5,6 +5,7 @@ import com.fiap.hackaton.domain.entity.Activity;
 public record ClassroomActivity(
         Long id,
         String name,
+        String description,
         boolean inGroup,
         Integer studentsPerGroup,
         Integer prestigeValue,
@@ -14,6 +15,7 @@ public record ClassroomActivity(
         this(
                 activity.getId(),
                 activity.getName(),
+                activity.getDescription(),
                 activity.isInGroup(),
                 activity.getStudentsPerGroup(),
                 activity.getPrestigeValue(),
